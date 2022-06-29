51d5002be26dd8d123bc12228960a7beaaf50a3b
saimanazir <annajammes9@gmail.com>
2022-06-29 13:48:08 +0500

    Latest commit 6/29/2022

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -3,15 +3,27 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class CommonMethods {
     public static WebDriver driver;
     public static void open ( String url){
+        FileReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+        switch (FileReader.getPropertyValue("browser")){
+            case "chrome":
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
-        driver.get(url);
+        break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver();
+                break;
+            default:throw new RuntimeException("invalid browser name");
+        }
+
+        driver.get(FileReader.getPropertyValue("url"));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
 
@@ -22,5 +34,6 @@ public class CommonMethods {
         if(driver!=null){
             driver.quit();
         }
-    }}
+    }
+}
 
